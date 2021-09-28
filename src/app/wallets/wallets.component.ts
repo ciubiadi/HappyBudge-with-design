8641aa1f56baed77b37d6b47e67743cff123bbc4
Ciubucciu Adrian Albert <ciubucciu.adrianalbert@gmail.com>
2021-09-28 22:45:07 +0300

    Notifications resolved

--- src/app/wallets/wallets.component.ts
+++ src/app/wallets/wallets.component.ts
@@ -64,13 +64,13 @@ export class WalletsComponent implements OnInit {
       this.walletsData.push(res);
     },
     err => {
-      alert("Something went wrong");
+      this.showNotification('Something went wrong', 'danger');
     })
   }
 
   deleteWallet(wallet: any) {
     this.walletsService.deleteWallet(wallet.id).subscribe(res => {
-      this.showNotification('Wallet Deleted Succesfully', 'danger');
+      this.showNotification('Wallet Deleted Succesfully', 'warning');
       this.getAllWallets();
     });
   }
@@ -102,13 +102,26 @@ export class WalletsComponent implements OnInit {
   }
 
   showNotification(msg, typeColor){
+    let iconType = '';
+    switch(typeColor) {
+      case 'danger':
+        iconType = 'error_outline';
+        break;
+      case 'warning':
+        iconType = 'delete_forever';
+        break;
+      case 'success':
+        iconType = 'task_alt';
+        break;
+      default:
+        iconType = 'notifications'
+    }
     $.notify({
-        icon: "notifications",
+        title: iconType,
         message: msg
-
     },{
         type: typeColor,
-        timer: 500,
+        timer: 50000,
         placement: {
             from: 'bottom',
             align: 'right'
@@ -116,8 +129,8 @@ export class WalletsComponent implements OnInit {
         template: 
         '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
           '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
-          '<i class="material-icons" data-notify="icon">notifications</i> ' +
-          '<span data-notify="title">{1}</span> ' +
+          '<i class="material-icons" data-notify="icon">{1}</i> ' +
+          // '<span data-notify="title"></span> ' +
           '<span data-notify="message">{2}</span>' +
           '<div class="progress" data-notify="progressbar">' +
             '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
